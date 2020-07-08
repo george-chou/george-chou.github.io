b6c36db04d024f2ed3af47e2b4cde9c9ed9a1f74
george-chou <georgechou21@foxmail.com>
2020-07-08 09:45:02 +0800

    Update browser.js

--- cv/assets/js/browser.js
+++ cv/assets/js/browser.js
@@ -1,4 +1,4 @@
-define(['device'], function(device) {
+define(['jquery', 'device'], function($, device) {
 
 function UpperFirstLetter(str)   
 {   
@@ -9,8 +9,7 @@ function getParenthesesStr(text)
 {
 	var result = ''; 
     if ($.isEmptyObject(text)) return result;
-	var regex = /\((.+?)\)/g;
-    var options = text.match(regex);
+    var options = text.match(/\((.+?)\)/g);
 	
 	if (!$.isEmptyObject(options)) 
 	{
@@ -27,15 +26,15 @@ function ParseWinVer(ver)
 { 
 	switch(Math.floor(10.0 * ver))
 	{
-		case 50: return "2000";
-		case 51: return "XP";
-		case 52: return "64-Bit Edition/Server 2003/Server 2003 R2";
-		case 60: return "Vista/Server 2008";
-		case 61: return "7/Server 2008 R2";
-		case 62: return "8/Server 2012";
-		case 63: return "8.1/Server 2012 R2";
+		case 50	: return "2000";
+		case 51	: return "XP";
+		case 52	: return "64-Bit Edition/Server 2003/Server 2003 R2";
+		case 60	: return "Vista/Server 2008";
+		case 61	: return "7/Server 2008 R2";
+		case 62	: return "8/Server 2012";
+		case 63	: return "8.1/Server 2012 R2";
 		case 100: return "10/Server 2016";
-		default: return "Unknown";
+		default	: return "Unknown";
 	}
 }
 
@@ -43,34 +42,34 @@ function ParseOS(t)
 {
 	switch(t.toLowerCase())
 	{
-		case "iphone/ios": return 0;
-		case "ipad/ios": return 1;
-		case "ipod/ios": return 2;
-		case "android": return 3; 
-		case "windows phone": return 5;
-		case "windows 10/server 2016": return 6;
-		case "windows 8.1/server 2012 r2": return 7;
-		case "windows 8/server 2012": return 8;
-		case "windows 7/server 2008 r2": return 9;
-		case "windows vista/server 2008": return 10;
-		case "windows 64-Bit edition/server 2003/server 2003 r2": return 11;
-		case "windows xp": return 12;
-		case "windows 2000": return 13;
-		case "macos": return 14;
-		case "linux": return 15;
-		default: return -1;
+		case "iphone/ios"										 : return 0;
+		case "ipad/ios"											 : return 1;
+		case "ipod/ios"											 : return 2;
+		case "android"											 : return 3; 
+		case "windows phone"									 : return 5;
+		case "windows 10/server 2016"							 : return 6;
+		case "windows 8.1/server 2012 r2"						 : return 7;
+		case "windows 8/server 2012"							 : return 8;
+		case "windows 7/server 2008 r2"							 : return 9;
+		case "windows vista/server 2008"						 : return 10;
+		case "windows 64-Bit edition/server 2003/server 2003 r2" : return 11;
+		case "windows xp"										 : return 12;
+		case "windows 2000"										 : return 13;
+		case "macos"											 : return 14;
+		case "linux"											 : return 15;
+		default													 : return -1;
 	}
 }
 
 function getOS()
-{
+{	
+	var agent = navigator.userAgent.toLowerCase(); 
+	
 	var os = {
 		type : device.os.toLowerCase(),
 		no 	 : -1,
 		ver  : -1
 	};
-	
-	var agent = navigator.userAgent.toLowerCase(); 
 	
 	if(os.type !== "unknown")
 	{				
@@ -105,8 +104,7 @@ function getOS()
 		}
 		else if(os.type == "windows")
 		{
-			var isPhone = (agent.indexOf("windows phone") > -1);
-			if(isPhone)
+			if(agent.indexOf("windows phone") > -1)
 			{
 				os.type = "Windows Phone";
 				os.ver = agent.match(/windows phone ([\d.]+)/)[1]; 
@@ -158,7 +156,6 @@ function getOS()
 
 function getBrowser()
 {
-	
 	var agent = navigator.userAgent.toLowerCase();
 	
 	var browser = {
@@ -250,14 +247,14 @@ function toBroICO(b)
 {
 	switch(parseInt(b))
 	{
-		case 0: return "opera";
-		case 1: return "firefox";
-		case 2: return "edge"; 
-		case 4: return "wechat";
-		case 16: return "chrome";
-		case 17: return "safari";
-		case 18: return "ie"; 
-		default: return "others";
+		case 0	: return "opera";
+		case 1	: return "firefox";
+		case 2	: return "edge"; 
+		case 4	: return "wechat";
+		case 16 : return "chrome";
+		case 17 : return "safari";
+		case 18 : return "ie"; 
+		default : return "others";
 	}
 }
 
@@ -265,14 +262,14 @@ function toBroName(b)
 {
 	switch(parseInt(b))
 	{
-		case 0: return isEn ? "Opera" : "欧朋";
-		case 1: return isEn ? "Firefox" : "火狐";
-		case 2: return "Edge"; 
-		case 4: return isEn ? "WeChat" : "微信";
-		case 16: return isEn ? "Chrome" : "谷歌";
-		case 17: return "Safari";
-		case 18: return "IE";  
-		default: return isEn ? "Others" : "其它";
+		case 0	: return isEn ? "Opera" : "欧朋";
+		case 1	: return isEn ? "Firefox" : "火狐";
+		case 2	: return "Edge"; 
+		case 4	: return isEn ? "WeChat" : "微信";
+		case 16 : return isEn ? "Chrome" : "谷歌";
+		case 17 : return "Safari";
+		case 18 : return "IE";  
+		default : return isEn ? "Others" : "其它";
 	}
 }
 
@@ -280,22 +277,22 @@ function toSysName(os)
 {
 	switch(parseInt(os))
 	{
-		case 0: return "iPhone";
-		case 1: return "iPad";
-		case 2: return "iPod";
-		case 3: return isEn ? "Android" : "安卓";
-		case 5: return "Win Phone";
-		case 6: return "Win10";
-		case 7: return "Win8.1";
-		case 8: return "Win8";
-		case 9: return "Win7";
-		case 10: return "Vista";
-		case 11: return "Win64";
-		case 12: return "WinXP";
-		case 13: return "Win2000";
-		case 14: return "MacOSX";
-		case 15: return "Linux"; 
-		default: return isEn ? "Unknown" : "未知";
+		case 0	: return "iPhone";
+		case 1	: return "iPad";
+		case 2	: return "iPod";
+		case 3	: return isEn ? "Android" : "安卓";
+		case 5	: return "Win Phone";
+		case 6	: return "Win10";
+		case 7	: return "Win8.1";
+		case 8	: return "Win8";
+		case 9	: return "Win7";
+		case 10 : return "Vista";
+		case 11 : return "Win64";
+		case 12 : return "WinXP";
+		case 13 : return "Win2000";
+		case 14 : return "MacOSX";
+		case 15 : return "Linux"; 
+		default : return isEn ? "Unknown" : "未知";
 	}
 }
 
@@ -303,22 +300,22 @@ function toSysICO(os)
 {
 	switch(parseInt(os))
 	{
-		case 0: return "ios";
-		case 1: return "ios";
-		case 2: return "ios";
-		case 3: return "android"; 
-		case 5: return "win10";
-		case 6: return "win10";
-		case 7: return "win10";
-		case 8: return "win10";
-		case 9: return "win7";
-		case 10: return "win7";
-		case 11: return "win32";
-		case 12: return "win32";
-		case 13: return "win32";
-		case 14: return "macos";
-		case 15: return "linux"; 
-		default: return "unknown";
+		case 0	: return "ios";
+		case 1	: return "ios";
+		case 2	: return "ios";
+		case 3	: return "android"; 
+		case 5	: return "win10";
+		case 6	: return "win10";
+		case 7	: return "win10";
+		case 8	: return "win10";
+		case 9	: return "win7";
+		case 10 : return "win7";
+		case 11 : return "win32";
+		case 12 : return "win32";
+		case 13 : return "win32";
+		case 14 : return "macos";
+		case 15 : return "linux"; 
+		default : return "unknown";
 	}
 }
 
